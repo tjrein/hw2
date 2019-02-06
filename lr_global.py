1a441d48ff5409163a2f51184a2642488852324a
Tom Rein <tomjrein@gmail.com>
2019-02-06 15:35:44 -0500

    computing linear regression

--- lr_global.py
+++ lr_global.py
@@ -1,29 +1,38 @@
 import numpy as np
+from numpy import linalg as LA
 from math import ceil
 
 np.set_printoptions(suppress=True)
 
+def linear_regression(data):
+    targets = data[:, 3:]
+    features = data[:, :3]
+
+    #test equation
+    #targets = np.array([ [6], [1], [9], [17], [12]])
+    #features = np.array([ [-1.1574], [-0.6751], [-0.1929], [0.7716], [1.2538] ])
+    #ones = np.ones((5, 1))
+    #features = np.append(ones, features, axis=1)
+
+
+    theta = LA.inv(features.T @ features) @ features.T @ targets
+    expected = features @ theta
+
+    return expected
+
 def standardize(data):
-
-    print("data", data[:, :2])
-    targets = data[:, 2: ]
-
+    targets = data[:, 2:]
     test = data[:, :2]
 
     mean = np.mean(test, axis=0)
     std = np.std(test, axis=0, ddof=1)
 
-    print("mean", mean)
-    print("std", std)
+    ones = np.ones((30, 1))
 
     standardized_data = (test - mean) / std
-
-    #print("standardized", standardized_data)
-    #targets.reshape((30, 1)
-    #print("targets shape", targets.shape)
+    standardized_data = np.append(ones, standardized_data, axis=1)
     standardized_data = np.append(standardized_data, targets, axis=1)
-    print("standardized_dat\n", standardized_data)
-    return
+
     return standardized_data
 
 def main():
@@ -32,19 +41,14 @@ def main():
     np.random.seed(0)
     np.random.shuffle(data)
 
-    print("len", len(data))
-
     range = ceil(len(data) * 2/3)
-
 
     training = data[0:range]
     test = data[range:]
 
-    print("training", training)
-    print("\n")
-
     training = standardize(training)
 
+    linear_regression(training)
 
 if __name__ == "__main__":
     main()
