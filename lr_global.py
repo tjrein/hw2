71ad2eafc152180bc4c8e1e90fc3530336562ab4
Tom Rein <tomjrein@gmail.com>
2019-02-06 17:08:49 -0500

    refactored

--- lr_global.py
+++ lr_global.py
@@ -4,36 +4,22 @@ from math import ceil
 
 np.set_printoptions(suppress=True)
 
-def linear_regression(data):
-    targets = data[:, 3:]
-    features = data[:, :3]
+def compute_rmse(targets, expected):
+    return np.sqrt(((targets - expected) ** 2).mean())
 
-    #test equation
-    #targets = np.array([ [6], [1], [9], [17], [12]])
-    #features = np.array([ [-1.1574], [-0.6751], [-0.1929], [0.7716], [1.2538] ])
-    #ones = np.ones((5, 1))
-    #features = np.append(ones, features, axis=1)
+def linear_regression(features, targets):
+    return LA.inv(features.T @ features) @ features.T @ targets
 
-
-    theta = LA.inv(features.T @ features) @ features.T @ targets
-    expected = features @ theta
-
-    return expected
-
-def standardize(data):
+def separate_data(data):
     targets = data[:, 2:]
-    test = data[:, :2]
+    features = data[:, :2]
+    return (targets, features)
 
-    mean = np.mean(test, axis=0)
-    std = np.std(test, axis=0, ddof=1)
-
-    ones = np.ones((30, 1))
-
-    standardized_data = (test - mean) / std
-    standardized_data = np.append(ones, standardized_data, axis=1)
-    standardized_data = np.append(standardized_data, targets, axis=1)
-
-    return standardized_data
+def standardize(features, mean=None, std=None):
+    features = (features - mean) / std
+    ones = np.ones((features.shape[0], 1))
+    features = np.append(ones, features, axis=1)
+    return features
 
 def main():
     data = np.genfromtxt('./x06Simple.csv', delimiter=',', dtype="uint16", skip_header=1, usecols=(1,2,3))
@@ -44,11 +30,23 @@ def main():
     range = ceil(len(data) * 2/3)
 
     training = data[0:range]
-    test = data[range:]
+    testing = data[range:]
 
-    training = standardize(training)
+    training_targets, training_features = separate_data(training)
+    testing_targets, testing_features = separate_data(testing)
 
-    linear_regression(training)
+    mean = np.mean(training_features, axis=0)
+    std = np.std(training_features, axis=0, ddof=1)
+
+    training_features = standardize(training_features, mean, std)
+    theta = linear_regression(training_features, training_targets)
+
+    testing_features = standardize(testing_features, mean, std)
+    expected = testing_features @ theta
+
+    rmse = compute_rmse(testing_targets, expected)
+
+    print(rmse)
 
 if __name__ == "__main__":
     main()
