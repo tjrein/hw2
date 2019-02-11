73bd727ee58938c480734a84e6aa6d9a819391cc
Tom Rein <tomjrein@gmail.com>
2019-02-11 12:53:26 -0500

    added s_folds

---
+++ s_folds.py
@@ -0,0 +1,67 @@
+import numpy as np
+from numpy import linalg as LA
+
+def compute_se(targets, expected):
+    return (targets - expected) ** 2
+
+def compute_rmse(se):
+    return np.sqrt(se.mean())
+
+def separate_data(data):
+    targets = data[:, 2:]
+    features = data[:, :2]
+    return (targets, features)
+
+def standardize(features, mean=None, std=None):
+    features = (features - mean) / std
+    ones = np.ones((features.shape[0], 1))
+    features = np.append(ones, features, axis=1)
+    return features
+
+def perform_s_folds(s, data):
+    se = []
+    for i in range(0, len(data)):
+        testing =  data[i]
+        training = [x for j, x in enumerate(data) if j != i]
+        training = np.vstack(training)
+
+        training_targets, training_features = separate_data(training)
+        testing_targets, testing_features = separate_data(testing)
+
+        mean = np.mean(training_features, axis=0)
+        std = np.std(training_features, axis=0, ddof=1)
+
+        training_features = standardize(training_features, mean, std)
+        testing_features = standardize(testing_features, mean, std)
+
+        theta = LA.inv(training_features.T @ training_features) @ training_features.T @ training_targets
+        expected = testing_features @ theta
+        error = compute_se(testing_targets, expected)
+        se.append(error)
+
+    se = np.vstack(se)
+    rmse = compute_rmse(se)
+    return rmse
+
+def main():
+    data = np.genfromtxt('./x06Simple.csv', delimiter=',', dtype="uint16", skip_header=1, usecols=(1,2,3))
+
+    s = 3
+
+    all_rmse = []
+    for i in range(0, 20):
+        np.random.seed(i)
+        np.random.shuffle(data)
+        split_data = np.array_split(data, s)
+        rmse = perform_s_folds(s, split_data)
+        all_rmse.append(rmse)
+
+
+    rmse_mean = np.mean(all_rmse, axis=0)
+    rmse_std = np.std(all_rmse, axis=0, ddof=1)
+
+
+    return
+
+if __name__ == '__main__':
+    main()
